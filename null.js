40671a17c1e05d33afa6cca66cd6db922e4f1878
MuhammadZikri <zikrim577@gmail.com>
2023-01-10 14:41:29 +0700

    Javascript - Null

---
+++ null.js
@@ -0,0 +1,16 @@
+
+// membuat variabel yang menampung valuenya null
+let lastname = null;
+
+// melakukan pengkondisian jika lastname sama dengan null
+if (lastname === undefined) {
+    // hasilnya 
+    console.info("Undefined");
+} else if (lastname === null) {
+    // hasilnya
+    console.info("Null");
+} else {
+    // hasilnya
+    console.info(lastname);
+}
+// outputnya : Null
---
+++ undefined.js
@@ -0,0 +1,16 @@
+
+// membuat variabel yang menampung valuenya null
+let middlename;
+
+// melakukan pengkondisian jika lastname sama dengan null
+if (middlename === undefined) {
+    // hasilnya 
+    console.info("Undefined");
+} else if (middlename === null) {
+    // hasilnya
+    console.info("Null");
+} else {
+    // hasilnya
+    console.info(middlename);
+}
+// outputnya : Null
